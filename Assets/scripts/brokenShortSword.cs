aeab497edfd24099a731ed04262bb99a265c1eeb
TheMannimal69 <william_biggs@outlook.com>
2021-09-23 16:12:19 -0500

    created grid, proper collison, robust AI

--- Assets/scripts/brokenShortSword.cs
+++ Assets/scripts/brokenShortSword.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class brokenShortSword : MonoBehaviour
+public class BrokenShortSword : MonoBehaviour
 {
+    [SerializeField] int damage = 1;
+
+    public int GetDamage() 
+    {
+        return damage;
+    }
+
+
 
 }
